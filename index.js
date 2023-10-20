e3c66af75ff20667402a9791414578f7e4faf254
iftekahrmahmud98@gmail.com <youremail@example.com>
2023-10-20 11:39:25 +0600

    added users api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -19,10 +19,6 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1tj4s8s.mongodb.net/?retryWrites=true&w=majority`;
 
-
-
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -33,14 +29,49 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+
     await client.connect();
-    // Send a ping to confirm a successful connection
+    const usersCollection = client.db('techDB').collection('users');
+    const servicesCollection =client.db('techDB').collection('services')
+
+    app.get("/users",  async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/services", async(req,res)=>{
+      const result =await servicesCollection.find().toArray()
+      res.send(result)
+    })
+    
+   app.get("/services/:id", async(req,res)=>{
+    const id =req.params.id;
+    const query ={_id : new ObjectId(id)}
+    const brandDetails = await servicesCollection.findOne(query)
+    res.send(brandDetails)
+   })
+   
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+
+    //await client.close();
   }
 }
 run().catch(console.dir);
@@ -49,19 +80,10 @@ run().catch(console.dir);
 
 
 
-
-
-
-
-
-
-
-
-
-app.get('/',(req,res)=>{
-     res.send('Apple server is running')
+app.get('/', (req, res) => {
+  res.send('Apple server is running')
 })
 
-app.listen(port,()=>{
-     console.log(`apple server is running on port ${port}`)
+app.listen(port, () => {
+  console.log(`apple server is running on port ${port}`)
 })
